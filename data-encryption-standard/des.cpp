1c7b0442921aff20925308e3494aa984c7944419
doraemon <a13432812701@163.com>
2024-10-10 17:09:18 +0800

    优化

--- data-encryption-standard/des.cpp
+++ data-encryption-standard/des.cpp
@@ -44,7 +44,7 @@ static const unsigned int sub_secret_key_table[SUB_SECRET_KEY_LEN] = {
 	34, 53, 46, 42, 50, 36, 29, 32};
 
 // 数据初始置换表，将数据进行重新排列
-static const unsigned char initial_permutation_table[TEXT_LEN] = {
+static const unsigned int initial_permutation_table[TEXT_LEN] = {
 	58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4,
 	62, 54, 46, 38, 30, 22, 14, 6, 64, 56, 48, 40, 32, 24, 16, 8,
 	57, 49, 41, 33, 25, 17, 9, 1, 59, 51, 43, 35, 27, 19, 11, 3,
@@ -52,21 +52,21 @@ static const unsigned char initial_permutation_table[TEXT_LEN] = {
 
 // 数据末置换表，将数据重新排列，其取值跟数据初始置换表有关
 // 即数据如果只经过初始置换表和末置换表，数据不会发生任何改变
-static const unsigned char final_permutation_table[TEXT_LEN] = {
+static const unsigned int final_permutation_table[TEXT_LEN] = {
 	40, 8, 48, 16, 56, 24, 64, 32, 39, 7, 47, 15, 55, 23, 63, 31,
 	38, 6, 46, 14, 54, 22, 62, 30, 37, 5, 45, 13, 53, 21, 61, 29,
 	36, 4, 44, 12, 52, 20, 60, 28, 35, 3, 43, 11, 51, 19, 59, 27,
 	34, 2, 42, 10, 50, 18, 58, 26, 33, 1, 41, 9, 49, 17, 57, 25};
 
 // 数据扩展置换表，将数据从32位扩展为48位，与子密钥的长度一致
-static const unsigned char extend_table[SUB_SECRET_KEY_LEN] = {
+static const unsigned int extend_table[SUB_SECRET_KEY_LEN] = {
 	32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9, 8, 9, 10, 11,
 	12, 13, 12, 13, 14, 15, 16, 17, 16, 17, 18, 19, 20, 21, 20, 21,
 	22, 23, 24, 25, 24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32, 1};
 
 // S盒置换表，共有8个子盒子，每个子盒子都将6位输入压缩为4位
 // 由于是由6位压缩为4位，则每个子盒子有 2^6 个元素，里面元素的最大值为 2^4-1
-static const unsigned char sbox_table[8][64] = {
+static const unsigned int sbox_table[8][64] = {
 	{// S1盒子
 	 14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7,
 	 0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8,
@@ -109,7 +109,7 @@ static const unsigned char sbox_table[8][64] = {
 	 2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11}};
 
 // P盒置换表
-static const unsigned char pbox_table[TEXT_LEN / 2] = {
+static const unsigned int pbox_table[TEXT_LEN / 2] = {
 	16, 7, 20, 21, 29, 12, 28, 17, 1, 15, 23, 26, 5, 18, 31, 10,
 	2, 8, 24, 14, 32, 27, 3, 9, 19, 13, 30, 6, 22, 11, 4, 25};
 
